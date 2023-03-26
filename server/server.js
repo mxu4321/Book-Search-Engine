ac595b6337669eb8f3704db0ddb37ae5f143d030
mengxue xu <mxu4321@gmail.com>
2023-03-25 22:25:36 -0700

    added appollo server & graphql in client json file

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-// ❄️: Implement the Apollo Server and apply it to the Express server as middleware.
+// ✅ Implement the Apollo Server and apply it to the Express server as middleware.
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
@@ -11,7 +11,7 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// ❄️: Create a new Apollo server and pass in the schema data.
+// ❄️ Create a new Apollo server and pass in the schema data.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-// ❄️: use Apollo Server instead of the Express routes
+// ❄️ use Apollo Server instead of the Express routes
 // app.use(routes);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
@@ -37,6 +37,6 @@ app.get("*", (req, res) => {
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  // ❄️: log the Apollo Server URL
+  // ❄️ log the Apollo Server URL
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
