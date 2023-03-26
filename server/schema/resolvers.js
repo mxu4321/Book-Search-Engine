76e57d8d027fd067367f0f84780bad902040ba3e
mengxue xu <mxu4321@gmail.com>
2023-03-25 22:19:16 -0700

    refactored auth in server

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,4 +1,5 @@
-// ❄️ Define the query and mutation functionality to work with the Mongoose models.
+// ✅ Define the query and mutation functionality to work with the Mongoose models.
+
 const { AuthenticationError } = require("apollo-server-express");
 const { User, Book } = require("../models");
 const { signToken } = require("../utils/auth");
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,5 +1,4 @@
-// TODO:
-// Update the auth middleware function to work with the GraphQL API.
+// ✅ Update the auth middleware function to work with the GraphQL API.
 
 const jwt = require('jsonwebtoken');
 
@@ -9,17 +8,21 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  // authMiddleware: function (req, res, next) { // <-- ❄️ this is the original code
+    authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    // ❄️ added `req.body.token ||` to the original code
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+    if (!token) { 
+      // return res.status(400).json({ message: 'You have no token!' });
+      // ❄️ original code⤴️, added return below
+      return req;
     }
 
     // verify token and get user data out of it
@@ -27,12 +30,16 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
+      // console.error(err);
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      // ❄️ commented out the original code below ⤵️
+      // return res.status(400).json({ message: 'invalid token!' });
     }
 
     // send to next endpoint
-    next();
+    // next();
+    // ❄️ original code⤴️, added return below
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
