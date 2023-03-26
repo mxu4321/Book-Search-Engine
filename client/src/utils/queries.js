0d45f2f8cb93eb478a67a42be03b1d45343b196d
mengxue xu <mxu4321@gmail.com>
2023-03-25 22:47:59 -0700

    set up query in queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,23 @@
-import { gql } from '@apollo/client';
+// ✅ This will hold the query GET_ME, which will execute the me query set up using Apollo Server.
 
-// queries.js: This will hold the query GET_ME, 
-// which will execute the me query set up using Apollo Server.
+import { gql } from "@apollo/client";
 
 export const GET_ME = gql`
-  query me {}`;
+# ----- ⏰TODO: check if 'query: me' is needed ------
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`;
